d4c2164028754fcb6e9baf90a16598347c3ff3b4
David Braga Rodrígues <dbragarodrigues@danielcastelao.com>
2022-03-16 13:41:11 +0100

    Estructura con la app básica

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,18 @@
 public class Main {
+        public static void main(final String[] args) {
+            final String token = args[0];
+            final DiscordClient client = DiscordClient.create(token);
+            final GatewayDiscordClient gateway = client.login().block();
 
+            gateway.on(MessageCreateEvent.class).subscribe(event -> {
+                final Message message = event.getMessage();
+                if ("!ping".equals(message.getContent())) {
+                    final MessageChannel channel = message.getChannel().block();
+                    channel.createMessage("Pong!").block();
+                }
+            });
+
+            gateway.onDisconnect().block();
+        }
+    }
 }
