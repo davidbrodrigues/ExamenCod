f782d503095529787d98489f9e5ee8d2266507a0
David Braga Rodrígues <dbragarodrigues@danielcastelao.com>
2022-05-05 18:17:31 +0200

    añadido embed comandos y tal + drive

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,4 @@
 //imports de las librerias de discordj4 necesarias
-import discord4j.common.util.Snowflake;
-import discord4j.core.DiscordClient;
-import discord4j.core.GatewayDiscordClient;
-import discord4j.core.event.domain.message.MessageCreateEvent;
-import discord4j.core.object.entity.Message;
-import discord4j.core.object.entity.channel.GuildMessageChannel;
-import discord4j.core.object.entity.channel.MessageChannel;
-import discord4j.core.spec.EmbedCreateSpec;
-import discord4j.core.spec.MessageCreateSpec;
-
 
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
@@ -22,25 +12,26 @@ import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.DriveScopes;
-import com.google.api.services.drive.model.File;
 import com.google.api.services.drive.model.FileList;
-
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import discord4j.core.DiscordClient;
+import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.entity.Message;
+import discord4j.core.object.entity.channel.MessageChannel;
+import discord4j.core.spec.EmbedCreateSpec;
+import discord4j.rest.util.Color;
+import java.io.*;
 import java.security.GeneralSecurityException;
+import java.time.Instant;
 import java.util.Collections;
 import java.util.List;
-
-import java.time.Instant;
 
 
 //codigo del bot de discord
 public final class Main {
 
     /** Application name. */
-    private static final String APPLICATION_NAME = "Google Drive API Java Quickstart";
+    private static final String APPLICATION_NAME = "BotDavid";
     /** Global instance of the JSON factory. */
     private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
     /** Directory to store authorization tokens for this application. */
@@ -50,7 +41,7 @@ public final class Main {
      * Global instance of the scopes required by this quickstart.
      * If modifying these scopes, delete your previously saved tokens/ folder.
      */
-    private static final List<String> SCOPES = Collections.singletonList(DriveScopes.DRIVE_METADATA_READONLY);
+    private static final List<String> SCOPES = Collections.singletonList(DriveScopes.DRIVE);
     private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
 
     /**
@@ -61,7 +52,7 @@ public final class Main {
      */
     private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
         // Load client secrets.
-        InputStream in = DriveQuickstart.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+        InputStream in = Main.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
         if (in == null) {
             throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
         }
@@ -74,7 +65,7 @@ public final class Main {
                 .setAccessType("offline")
                 .build();
         LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
-        Credential credential = new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+        Credential credential = new AuthorizationCodeInstalledApp(flow, receiver).authorize("53067492883-lgmtusf8mvl00ko3p1caldtdnhtcdse0.apps.googleusercontent.com");
         //returns an authorized Credential object.
         return credential;
     }
@@ -84,63 +75,103 @@ public final class Main {
         final DiscordClient client = DiscordClient.create(token);
         final GatewayDiscordClient gateway = client.login().block();
 
-        gateway.on(MessageCreateEvent.class).subscribe(event -> {
-            final Message message = event.getMessage();
-            if ("!ping".equals(message.getContent())) {
-                final MessageChannel channel = message.getChannel().block();
-                channel.createMessage("Pong!").block();
-            }
-        });
 
         EmbedCreateSpec embed = EmbedCreateSpec.builder()
                 .color(Color.BLUE)
-                .title("Title")
-                .url("https://discord4j.com")
-                .author("Some Name", "https://discord4j.com", "https://i.imgur.com/F9BhEoz.png")
-                .description("a description")
-                .thumbnail("https://i.imgur.com/F9BhEoz.png")
+                .title("Embed bot de cod")
+                .url("https://www.youtube.com/watch?v=z6-voch3JIk&ab_channel=DiscordBots")
+                .author("David", "https://media.redadn.es/series/capi/capitulo_14058.jpg", "https://i.imgur.com/LshAsmQ.jpeg")
+                .description("No hay descripcion")
+                .thumbnail("https://i.imgur.com/EASQgbY.jpeg")
                 .addField("field title", "value", false)
                 .addField("\u200B", "\u200B", false)
                 .addField("inline field", "value", true)
                 .addField("inline field", "value", true)
                 .addField("inline field", "value", true)
-                .image("https://i.imgur.com/F9BhEoz.png")
+                .image("https://i.imgur.com/tiibS57.jpeg")
                 .timestamp(Instant.now())
-                .footer("footer", "https://i.imgur.com/F9BhEoz.png")
+                .footer("footer", "https://i.imgur.com/QOaCZiE.jpeg")
                 .build();
 
-        client.getChannelById(Snowflake.of(CHANNEL_ID))
-                .ofType(GuildMessageChannel.class)
-                .flatMap(channel -> channel.createMessage(embed))
-                .subscribe();
+        gateway.on(MessageCreateEvent.class).subscribe(event -> {
+            final Message message = event.getMessage();
+            //si el mensaje recibido es !ping
+            if ("!ping".equals(message.getContent())) {
+                final MessageChannel channel = message.getChannel().block();
+                //devolver un !Pong
+                channel.createMessage("Pong!").block();
+            }
 
-        Snowflake channelId;
-        client.getChannelById(channelId)
-                .ofType(GuildMessageChannel.class)
-                .flatMap(channel -> channel.createMessage(MessageCreateSpec.builder()
-                        .content("A meaningful quote from a good movie")
-                        .addEmbed(embed)
-                        .build()
-                )).subscribe();
+            //si el mensaje recibido es hola nos pone el embed
+            if ("hola".equalsIgnoreCase(message.getContent())){
+                final MessageChannel channel = message.getChannel().block();
+                channel.createMessage(embed).block();
+            }
 
-        Color rgbColor = Color.of(rr, gg, bb);
-
-        Color hexColor = Color.of(0xAABBCC);
-
-        EmbedCreateSpec embed = EmbedCreateSpec.builder()
-    ...
-    .image("attachment://file-name.png")
-                .build();
-
-        client.getChannelById(channelId)
-                .ofType(GuildMessageChannel.class)
-                .flatMap(channel -> channel.createMessage(MessageCreateSpec.builder()
-                        .content("content? content")
-                        .addFile("file-name.png", fileAsInputStream)
-                        .addEmbed(embed)
-                        .build()
-                )).subscribe();
-
+            //si el mensaje es !files nos devuelve sus nombres
+            if ("!files".equals(message.getContent())){
+                File file = new File("/home/dam1/cod/imagenes");
+                File files[] = file.listFiles();
+                for (int i = 0; i < files.length; i++) {
+                    final MessageChannel channel = message.getChannel().block();
+                    channel.createMessage(files[i].getName()).block();
+                }
+            }
+            //si el mensaje es !descarga
+            if("!descarga".equals(message.getContent())){
+                //hace el build de un nuevo servicio autorizado de la API
+                final NetHttpTransport HTTP_TRANSPORT;
+                try {
+                    HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+                    Drive service = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+                            .setApplicationName(APPLICATION_NAME)
+                            .build();
+                    //busca una capeta llamada BotImagenes dentro del drive
+                    FileList result = service.files().list()
+                            .setQ("name contains 'BotImagenes' and mimeType = 'application/vnd.google-apps.folder'")
+                            .setSpaces("drive")
+                            .setFields("nextPageToken, files(id, name)")
+                            .execute();
+                    List<com.google.api.services.drive.model.File> files = result.getFiles();
+                    //si no existe la carpeta nos avisa, saliendo por pantalla No files found
+                    if (files == null || files.isEmpty()) {
+                        System.out.println("No files found.");
+                    } else { //si la carpeta si existe
+                        String dirImagenes = null;
+                        System.out.println("Files:");
+                        for (com.google.api.services.drive.model.File file : files) {
+                            System.out.printf("%s (%s)\n", file.getName(), file.getId());
+                            dirImagenes = file.getId(); //guarda el nombre y el Id de la carpeta en el String dirImagenes
+                        }
+                        //busca la imagen
+                        FileList resultImagenes= service.files().list()
+                                .setQ("name contains 'image' and parents in '" + dirImagenes + "'")
+                                .setSpaces("drive")
+                                .setFields("nextPageToken, files(id, name)")
+                                .execute();
+                        List<com.google.api.services.drive.model.File> filesImagenes = resultImagenes.getFiles();
+                        //si no encuentra salta No image found
+                        if(filesImagenes == null || filesImagenes.isEmpty())
+                            System.out.println("No image found.");
+                        else{ //si encuentra
+                            for (com.google.api.services.drive.model.File file : filesImagenes) {
+                                System.out.printf("Imagen: %s\n", file.getName());
+                                //guarda el 'stream' dentro del direcotrio aux.jpeg que tiene que existir
+                                OutputStream outputStream = new FileOutputStream("/home/dam1/cod/examen/aux.jpeg");
+                                service.files().get(file.getId())
+                                        .executeMediaAndDownloadTo(outputStream);
+                                outputStream.flush();
+                                outputStream.close();
+                            }
+                        }
+                    }
+                } catch (GeneralSecurityException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
         gateway.onDisconnect().block();
     }
 }
